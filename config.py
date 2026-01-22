2c60b161d45207a4878f499af23c20aaafb74ec3
Travis Wessman <traviswessman26@gmail.com>
2026-01-22 15:12:34 -0500

    database init + config
    First round of coding. Following docs created company specific settings and database. The config.py file stores all app settings that may change or need to be updated in the future. No need to search just find all repeated information within this file. Initialized the databases for leads and call_log. Storing every customer who calls through an id. Saves phone number name and any other provided information. Also call_log to track who has already been texted.

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from datetime import timedelta
+
+class Config:
+    
+    # database variable
+    DATABASE_PATH = 'leads.db'
+
+    # flask
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-later'
+
+    # business + notification settings
+    BUSINESS_NAME = "Alternative Heating Solutions" 
+    PERSONAL_PHONE = "508-328-9372"
+    TWILIO_PHONE = "XXX-XXX-XXXX"
+
+    # auto-text settings
+    RESPONSE_TIME_HOURS = 3
+    DUPLICATE_TEXT_WINDOW = timedelta(hours=24) # don't text back same number within 24hrs
